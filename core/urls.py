3b6f320ebb3b4ba67e05672afe3e1802b9790f0e
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-25 15:12:36 +0430

    start editing home template

--- core/urls.py
+++ core/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('', include('dashboard.urls')),
+    path('dashboard', include('dashboard.urls')),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
